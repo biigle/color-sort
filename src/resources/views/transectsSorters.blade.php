c3a74a4b1fccfae0ce6985670291f245befc23d3
Martin Zurowietz <martin@zurowietz.de>
2016-07-15 09:46:37 +0200

    Display message if no colors are available for sorting

--- src/resources/views/transectsSorters.blade.php
+++ src/resources/views/transectsSorters.blade.php
@@ -1,8 +1,5 @@
 <span class="list-group-item color-sort-list-group-item" title="Sort images by color" data-ng-controller="SortByColorController">
     <div class="clearfix">
-        Color
-        <span class="text-muted" data-ng-show="isFetchingColors()">(fetching colors...)</span>
-        <span class="text-muted" data-ng-show="isComputingNewColor()">(computing new...)</span>
         @can('edit-in', $transect)
             @if (Copria::userHasKey())
                 <span class="pull-right" data-ng-if="canRequestNewColor()">
@@ -13,6 +10,10 @@
                 </span>
             @endif
         @endcan
+        Color
+        <span class="text-muted ng-hide" data-ng-show="isFetchingColors()">(fetching colors...)</span>
+        <span class="text-muted ng-hide" data-ng-show="isComputingNewColor()">(computing new...)</span>
+        <span class="text-muted ng-hide" data-ng-show="!hasColors() && !isFetchingColors()">(none available)</span>
     </div>
     <ul class="list-unstyled available-colors-list" data-ng-if="hasColors()">
         <li class="available-colors-item" data-ng-repeat="color in getColors()" style="background-color:#@{{color}};" title="Sort by this color" data-ng-click="toggle(color)" data-ng-class="{active: active(color)}"></li>
