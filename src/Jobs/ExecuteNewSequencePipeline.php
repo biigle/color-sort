159a4bc8b94a8c00127067717146e05cf347d459
Martin Zurowietz <martin@zurowietz.de>
2016-08-25 13:54:20 +0200

    Improve fetching of image data for the compute job

--- src/Jobs/ExecuteNewSequencePipeline.php
+++ src/Jobs/ExecuteNewSequencePipeline.php
@@ -3,14 +3,14 @@
 namespace Dias\Modules\Copria\ColorSort\Jobs;
 
 use DB;
-use Dias\Jobs\Job;
-use Dias\Modules\Copria\ColorSort\Sequence;
 use Dias\User;
-use Dias\Modules\Copria\User as CopriaUser;
+use Dias\Jobs\Job;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Dias\Modules\Copria\PipelineCallback;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Dias\Modules\Copria\ColorSort\Sequence;
+use Dias\Modules\Copria\User as CopriaUser;
 use Dias\Modules\Copria\ColorSort\Http\Controllers\Api\TransectColorSortSequenceController;
 
 class ExecuteNewSequencePipeline extends Job implements ShouldQueue
@@ -56,14 +56,17 @@ class ExecuteNewSequencePipeline extends Job implements ShouldQueue
         $callback->function = TransectColorSortSequenceController::class.'@result';
         $callback->payload = ['id' => $this->sequence->id];
 
-        $transect = $this->sequence->transect()->with('images')->first();
+        $transect = $this->sequence->transect()->first();
+        $images = $transect->images()
+            ->orderBy('id')
+            ->pluck('filename', 'id');
 
         CopriaUser::convert($this->user)
             ->executeCopriaPipeline(config('copria_color_sort.pipeline_id'), $callback->url, [
                 config('copria_color_sort.hex_color_selector') => $this->sequence->color,
                 config('copria_color_sort.images_directory_selector') => $transect->url,
-                config('copria_color_sort.images_filenames_selector') => $transect->images->pluck('filename')->implode(','),
-                config('copria_color_sort.images_ids_selector') => $transect->images->pluck('id')->implode(','),
+                config('copria_color_sort.images_filenames_selector') => $images->values()->implode(','),
+                config('copria_color_sort.images_ids_selector') => $images->keys()->implode(','),
                 config('copria_color_sort.target_url_selector') => $callback->url,
             ]);
 
